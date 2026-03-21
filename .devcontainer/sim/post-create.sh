447cfe7063f46120fe4da234ed0fe2f154aa9eab
Tal Glanzman <talglanzman@gmail.com>
2026-03-21 17:36:12 +0000

    install deps in startup

--- .devcontainer/sim/post-create.sh
+++ .devcontainer/sim/post-create.sh
@@ -7,3 +7,4 @@ git submodule update --init --recursive
 
 conda env create -f submodules/isca/ci/environment-py3.12_frozen.yml
 conda run -n isca_env pip install -e submodules/isca/src/extra/python
+pip install -e submodules/isca/src/extra/python
