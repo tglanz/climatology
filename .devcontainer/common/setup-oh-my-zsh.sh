03b29325d2bc4fc2bd81d50821c3dac3b66ea716
Tal Glanzman <talglanzman@gmail.com>
2026-04-04 08:23:06 +0000

    more fixes

--- .devcontainer/common/setup-oh-my-zsh.sh
+++ .devcontainer/common/setup-oh-my-zsh.sh
@@ -4,6 +4,7 @@ set -e
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 cat > "$HOME/.zshrc" <<'EOF'
+# ==== BEGIN: by setup-oh-my-zsh.sh ========
 export ZSH="$HOME/.oh-my-zsh"
 
 ZSH_THEME="robbyrussell"
@@ -11,7 +12,5 @@ ZSH_THEME="robbyrussell"
 plugins=(git)
 
 source $ZSH/oh-my-zsh.sh
-
-# Fix tmux with Ghostty terminal (xterm-ghostty terminfo not available on remote)
-alias tmux='TERM=xterm-256color tmux'
+# ==== END: setup-oh-my-zsh.sh ========
 EOF
