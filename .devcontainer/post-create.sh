055ae679a62b3acb19a4e8a8c44d913a09fe48a5
Tal Glanzman <talglanzman@gmail.com>
2026-04-26 17:21:51 +0000

    don't base by default

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -6,5 +6,11 @@ bash .devcontainer/common/setup-dotfiles.sh
 git submodule update --init --recursive
 
 export PATH="/home/ubuntu/miniforge3/bin:$PATH"
+
+# Prevent conda from activating the base environment on shell start.
+# Without this, running any conda command auto-sources profile.d/conda.sh
+# and drops you into (base).
+conda config --set auto_activate_base false
+
 conda env create -f submodules/isca/ci/environment-py3.12_frozen.yml
 conda run -n isca_env pip install -e submodules/isca/src/extra/python
